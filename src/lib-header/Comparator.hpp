70c8fbf10ed820556654a1308f60f7e0f640ea2d
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-02-28 11:31:45 +0700

    chore: add function args

---
+++ src/lib-header/Comparator.hpp
@@ -0,0 +1,38 @@
+#ifndef _COMPARATOR_HPP_
+#define _COMPARATOR_HPP_
+
+class Comparator
+{
+public:
+    // Getter & Setter
+    void setIndex();
+    void setCombinationType();
+
+    // TODO: complete methods args
+    /**
+     * Get the strongest combination deck
+     * @param Deck* arr_of_deck
+     * @return int index of the strongest combination in arr_of_deck
+     */
+    int getStrongestCombination();
+
+    /**
+     * Get the strongest combination deck
+     * @param Deck* arr_of_deck
+     * @return double constant of the strongest combination in arr_of_deck
+     */
+    double getStrongestCombinationType();
+
+    /**
+     * Get the strongest combination deck
+     * @param Deck* arr_of_deck
+     * @return Comparator object contain index of the strongest combination & combinationConstant
+     */
+    Comparator compare();
+    // Just in case this class needs to be extends
+protected:
+    int index;
+    int combinationConstant;
+};
+
+#endif;
