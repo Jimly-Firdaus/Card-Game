209dd2c3b1f3f08ac9cae35d5631bcd6bd3590b4
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-03-02 00:28:17 +0700

    feat: add Javadoc comment

--- src/Ability/Ability.hpp
+++ src/Ability/Ability.hpp
@@ -4,7 +4,18 @@
 
 class Ability{
     public:
+        /**
+        * Call card ability. (pure virtual)
+        * @param {player&} user Owner of the card
+        * @param {player&} target Target player
+        * @param {vector<player>&} playerOrder Player turn order
+        * @param {int&} tableReward Current table reward
+        * those parameters is written for ability usage completion, it can be user or not according to the need of the program.
+        */
         virtual void callCard() = 0;
+        /**
+         * Getting card information. (pure virtual)
+         */
         virtual void cardInfo() = 0;
 };
 
