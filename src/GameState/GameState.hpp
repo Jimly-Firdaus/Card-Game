7ccc9b82617dbe838aee9864ff14a2879c3a0125
billc27 <110593711+billc27@users.noreply.github.com>
2023-03-01 23:34:20 +0700

    added comments for GameState.hpp

--- src/GameState/GameState.hpp
+++ src/GameState/GameState.hpp
@@ -8,25 +8,27 @@ class GameState {
         int playerOrder[7];
 
     public:
+        // Ctor
         GameState();
 
-        int getRewardPoint();
-        void setRewardPoint(int);
+        // Getter & Setter
+        int getRewardPoint(); // Get game's current reward point
+        void setRewardPoint(int); // Set game's current reward point with desired value
+        void setTableCard(char *arrayOfCard); // Set tableCard with values from arrayOfCard
+        void setPlayerOrder(int *arrayOfOrder); // Set playerOrder with values from arrayOfCard
 
-        void showTableCard();
-        void setTableCard(char *arrayOfCard);
-
-        void showPlayerOrder();
-        void setPlayerOrder(int *arrayOfOrder);
-
-        void fourtimesRewardPoint();
-        void doubleRewardPoint();
-        void halfRewardPoint();
-        void quarterRewardPoint();
+        void showTableCard(); // Print Table Card
+        void showPlayerOrder(); // Print Player Order
+        
+        void fourtimesRewardPoint(); // Quadruple game's current reward point
+        void doubleRewardPoint(); // Double game's current reward point
+        void halfRewardPoint(); // Halve game's current reward point
+        void quarterRewardPoint(); // Quarter game's current reward point
 
         void nextPlayerOrder(); // Player Order Rotation
         
-        // bool isWin(); Wait for Player
+        // bool isWin();
+        // Wait for Player
 };
 
 #endif
