91dcf8d09dddfc7322727aec92843e868a71533e
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-03-04 11:10:33 +0700

    fix header

--- src/Deck/Deck.hpp
+++ src/Deck/Deck.hpp
@@ -7,31 +7,29 @@ using namespace std;
 
 class Deck
 {
-private:
-    pair<int, char> deckCard[52];
-    pair<int, char> tableCard[5];
-    char abilityCard[7];
-
 public:
     // Constructor
     Deck();
 
     // Setter Deck Card
-    void setDeckCard(pair<int, char> *arrayOfCard);
+    void setDeckCard();
     pair<int, char> *getRandomCard();
     pair<int, char> *getCardFromFile();
 
-    // Randomize while
     // Getter Deck Card
     pair<int, char> *getDeckCard();
 
-    // Show top deck card
-    void showTopDeckCard();
-
+    /*
     // Swap Deck Card and Player Card
     pair<int, char> takeADeckCard();
     void returnADeckCard(pair<int, char>);
     void shuffleDeckCard(); // Perlu ga? waktu kembaliin kartu habis swap taruh paling bawah aja atau shuffle lagi
+    */
+
+private:
+    pair<int, char> *deckCard;
+    pair<int, char> *tableCard;
+    char *abilityCard;
 };
 
 #endif
