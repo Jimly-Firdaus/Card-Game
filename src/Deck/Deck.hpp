dfa361aa2ed2a5b1dabb442344c5a5ae8ae71658
munzayanahusn <husniamunzayana.7802@gmail.com>
2023-03-12 01:19:44 +0700

    add header

--- src/Deck/Deck.hpp
+++ src/Deck/Deck.hpp
@@ -1,45 +1,65 @@
 #ifndef DECK_HPP
 #define DECK_HPP
 
+#define vector Vector
 #include <iostream>
-#include <utility>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <fstream>
+
 using namespace std;
 
 class Deck
 {
 public:
     // Constructor
-    Deck();
-
-    // Setter Deck Card
-    void setDeckCard();
-    pair<int, char> *getRandomCard(pair<int, char> *card);
-    pair<int, char> *getCardFromFile(string fileName);
-
-    // Getter Deck Card
-    pair<int, char> *getDeckCard();
-
-    /*
-    // Swap Deck Card and Player Card
-    pair<int, char> takeADeckCard();
-    void returnADeckCard(pair<int, char>);
-    void shuffleDeckCard(); // Perlu ga? waktu kembaliin kartu habis swap taruh paling bawah aja atau shuffle lagi
+    Deck();                                                                                                     // Default Constructor
+    Deck(vector<pair<int, char> > deckCards, vector<pair<int, char> > tableCards, vector<string> abilityCards); // User-defined Constructor
+    /* NOTE :
+        Boleh ga ya kalo Default Constructor buat Random Shuffle &&
+        User-defined Constructor untuk Read Deck Card Form File
     */
+
+    // Getter
+    vector<pair<int, char> > getDeckCard();
+    vector<pair<int, char> > getTableCard();
+    vector<string> getAbilityCard();
+
+    /* NOTE : Atau AbilityCard mau dibikin pair sama status : udah kepake/belum */
+
+    // Setter
+    void setDeckCard(vector<pair<int, char> > newDeckCards);
+    void setTableCard(vector<pair<int, char> > newTableCards);
+    void setAbilityCard(vector<string> newAblitiCards);
+
+    // Generate Card From File
+    void cardFromFile(string filePath);
+
+    // Validasi Card From File
+    bool validDeckCard(vector<pair<int, char> > deckCards);
+
+    // Shuffle Card
+    template <class T>
+    void shuffleCard(T card);
+
+    // Take A Deck Card
+    pair<int, char> getADeckCard(vector<pair<int, char> > deckCards);
+
+    // Return A Deck Card
+    void retDeckCard(pair<int, char> card);
+
+    // PrintCard
+    void printCard();
 
 private:
-    pair<int, char> *deckCard;
-    pair<int, char> *tableCard;
-    const string abilityCard[7] = {"Re-Roll", "Quadruple", "Quarter", "Reverse", "Swap", "Switch", "Abilityless"};
-    pair<int, char> *card;
-    /*
-    make_pair(1,'M'), make_pair(2,'M'), make_pair(3,'M'), make_pair(4,'M'), make_pair(5,'M'), make_pair(6,'M'), make_pair(7,'M'), make_pair(8,'M'), make_pair(9,'M'), make_pair(10,'M'), make_pair(11,'M'), make_pair(12,'M'), make_pair(13,'M'),
-    make_pair(1,'B'), make_pair(2,'B'), make_pair(3,'B'), make_pair(4,'B'), make_pair(5,'B'), make_pair(6,'B'), make_pair(7,'B'), make_pair(8,'B'), make_pair(9,'B'), make_pair(10,'B'), make_pair(11,'M'), make_pair(12,'M'), make_pair(13,'M'),
-    make_pair(1,'M'), make_pair(2,'M'), make_pair(3,'M'), make_pair(4,'M'), make_pair(5,'M'), make_pair(6,'M'), make_pair(7,'M'), make_pair(8,'M'), make_pair(9,'M'), make_pair(10,'M'), make_pair(11,'M'), make_pair(12,'M'), make_pair(13,'M'),
-    make_pair(1,'M'), make_pair(2,'M'), make_pair(3,'M'), make_pair(4,'M'), make_pair(5,'M'), make_pair(6,'M'), make_pair(7,'M'), make_pair(8,'M'), make_pair(9,'M'), make_pair(10,'M'), make_pair(11,'M'), make_pair(12,'M'), make_pair(13,'M')
-    */
+    vector<pair<int, char> > deckCards;
+    vector<pair<int, char> > tableCards;
+    vector<string> abilityCards;
+    // vector<string> abilityCards = {"Re-Roll", "Quadruple", "Quarter", "Reverse", "Swap", "Switch", "Abilityless"};
+    // no instance of constructor "std::Vector<_Tp, _Alloc>::Vector [with _Tp=std::string, _Alloc=std::allocator<std::string>]" matches the argument list
 };
-
 #endif
 
 /*
