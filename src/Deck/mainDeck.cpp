f1ef2d815b7a1908c738dc52334af60cf472768b
munzayanahusn <husniamunzayana.7802@gmail.com>
2023-03-12 01:20:14 +0700

    dummy main

---
+++ src/Deck/mainDeck.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Deck.hpp"
+using namespace std;
+
+int main()
+{
+    vector<string> abilityCards;
+    abilityCards.push_back("Re-Roll");
+    abilityCards.push_back("Quadruple");
+    abilityCards.push_back("Quarter");
+    abilityCards.push_back("Reverse");
+    abilityCards.push_back("Swap");
+    abilityCards.push_back("Switch");
+    abilityCards.push_back("Abilityless");
+
+    string cek;
+    while (cek in abilityCards)
+    {
+        cout << cek << endl;
+    }
+}
