b01a73fe776b60881f8b1ff1d0de8ca3f718da86
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-12 12:48:48 +0700

    fix: add more comparation

--- src/lib-header/Comparator.hpp
+++ src/lib-header/Comparator.hpp
@@ -1,9 +1,21 @@
-#ifndef _COMPARATOR_HPP_
-#define _COMPARATOR_HPP_
+#ifndef COMPARATOR_HPP
+#define COMPARATOR_HPP
+
+#include "Deck.hpp"
+#include "Exception.hpp"
+#include <map>
+using namespace std;
+
+const int MAX_AVAIL_NUMBER = 13;
+const int MAX_AVAIL_CHAR = 4;
+const int MAX_PLAYER_CARD = 7;
 
 class Comparator
 {
 public:
+    // Constructor
+    Comparator();
+
     // Getter & Setter
     void setIndex();
     void setCombinationType();
@@ -11,17 +23,19 @@ public:
     // TODO: complete methods args (missing Deck type)
     /**
      * Get the strongest combination deck
-     * @param Deck* arr_of_deck
-     * @return int index of the strongest combination in arr_of_deck
+     * @param Deck game cards
+     * @param Deck corresponding player cards
+     * @return float value of the strongest combination in playerCards
      */
-    int getStrongestCombination();
+    float getStrongestCombination(Deck gameCard, Deck playerCard);
 
     /**
      * Get the strongest combination deck
-     * @param Deck* arr_of_deck
-     * @return double constant of the strongest combination in arr_of_deck
+     * @param Deck game cards
+     * @param Deck* player cards
+     * @return float constant of the strongest combination in arr_of_deck
      */
-    double getStrongestCombinationType();
+    float getStrongestCombinationType();
 
     /**
      * Get the strongest combination deck
@@ -29,10 +43,26 @@ public:
      * @return Comparator object contain index of the strongest combination & combinationConstant
      */
     Comparator compare();
+
+    float searchVal(int number, char type);
+
+    bool isStraightFlush(Deck tableCard, Deck playerCard);
+    bool isFourKind(Deck tableCard, Deck playerCard);
+    bool isFullHouse(Deck tableCard, Deck playerCard);
+    bool isFlush(Deck tableCard, Deck playerCard);
+    pair<bool, float> isStraight(Deck tableCard, Deck playerCard);
+    bool isThreeKind(Deck tableCard, Deck playerCard);
+    bool isTwoPair(Deck tableCard, Deck playerCard);
+    bool isPair(Deck tableCard, Deck playerCard);
     // Just in case this class needs to be extends
 protected:
     int index;
     int combinationConstant;
+
+    // look up table for constants
+    map<pair<int, char>, float> valueTable;
+    char avail_chars[4];
+    float baseValue = 0.1;
 };
 
-#endif;
+#endif
