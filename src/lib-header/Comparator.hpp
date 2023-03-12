9a52bda35bfa87b7358c6c7f4cd54174bea801e3
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-12 19:47:11 +0700

    refactor: add doxygen to interface

--- src/lib-header/Comparator.hpp
+++ src/lib-header/Comparator.hpp
@@ -6,6 +6,7 @@
 #include <map>
 using namespace std;
 
+// MAX CARD SETTINGS
 const int MAX_AVAIL_NUMBER = 13;
 const int MAX_AVAIL_CHAR = 4;
 const int MAX_PLAYER_CARD = 7;
@@ -44,17 +45,75 @@ public:
      */
     Comparator compare();
 
+    /**
+     * Give the constant valuation of given number & type (color)
+     * @param number 
+     * @param type 
+     * @return float constant
+     */
     float searchVal(int number, char type);
-
+    /**
+     * Check whether if current hand + table card is Straight Flush
+     * @param tableCard table card (5 total)
+     * @param playerCard player hand (2 total)
+     * @return pair<bool, float> bool for Straight Flush. If true returns the valuation, else returns 0.0 
+     */
     pair<bool, float> isStraightFlush(Deck tableCard, Deck playerCard);
+    /**
+     * Check whether if current hand + table card is Four of Kind
+     * @param tableCard table card (5 total)
+     * @param playerCard player hand (2 total)
+     * @return pair<bool, int> bool for Four of Kind. If true returns the card number, else returns -1 
+     */
     pair<bool, int> isFourKind(Deck tableCard, Deck playerCard);
     bool isFullHouse(Deck tableCard, Deck playerCard);
+    /**
+     * Check whether if current hand + table card is Flush
+     * @param tableCard table card (5 total)
+     * @param playerCard player hand (2 total)
+     * @return pair<bool, char> bool for Flush. If true returns the card type (color), else returns empty char
+     */
     pair<bool, char> isFlush(Deck tableCard, Deck playerCard);
+    /**
+     * Check whether if current hand + table card is Straight
+     * @param tableCard table card (5 total)
+     * @param playerCard player hand (2 total)
+     * @return pair<bool, float> bool for Straight. If true returns the valuation, else returns 0.0 
+     */
     pair<bool, float> isStraight(Deck tableCard, Deck playerCard);
+    /**
+     * Check whether if current hand + table card is Straight
+     * @param mergedCard merged card (7 total)
+     * @return pair<bool, float> bool for Straight. If true returns the valuation, else returns 0.0  
+     */
     pair<bool, float> isStraight(Deck mergedCard);
+    /**
+     * Give current hand combination value if current hand + table card is Straight
+     * @param mergedCard merged card (7 total)
+     * @return pair<bool, float> bool for Straight. If true returns the valuation, else returns 0.0  
+     */
     pair<bool, float> straightComparator(Deck mergedCard);
+    /**
+     * Find the biggest possibly type among the same numbers
+     * @param v merged cards
+     * @param number given number
+     * @return pair<int, char> 
+     */
     pair<int, char> findBiggest (vector<pair<int, char>> v, int number);
+    /**
+     * Merge 2 given Decks
+     * @param firstDeck 
+     * @param secondDeck 
+     * @return vector<pair<int, char>> vector containing 2 merged Decks content 
+     */
     vector<pair<int, char>> mergeDeck (Deck firstDeck, Deck secondDeck);
+    /**
+     * Merge 2 given Decks
+     * @param firstDeck 
+     * @param secondDeck 
+     * @return vector<pair<int, char>> vector containing 2 merged Decks content 
+     */
+    vector<pair<int, char>> mergeDeck(vector<pair<int, char>> firstDeck, vector<pair<int, char>> secondDeck);
     vector<pair<int, char>> findPair (Deck tableCard, Deck playerCard);
     bool isThreeKind(Deck tableCard, Deck playerCard);
     bool isTwoPair(Deck tableCard, Deck playerCard);
@@ -63,7 +122,6 @@ public:
 protected:
     int index;
     int combinationConstant;
-
     // look up table for constants
     map<pair<int, char>, float> valueTable;
     char avail_chars[4];
