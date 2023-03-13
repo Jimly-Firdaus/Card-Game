3342746af468048cea06d555a01aaa3e3137e376
asyifashfr <asyifashfr.com>
2023-03-13 17:27:04 +0700

    Revisi

--- src/player/playerAction.hpp
+++ src/player/playerAction.hpp
@@ -1,7 +1,7 @@
 #ifndef PLAYERACTION_HPP
 #define PLAYERACTION_HPP
 
-#include "player.hpp"
+#include "Player.hpp"
 
 /*Function overriding here
 Class: PlayerAction extends dari Player buatKombinasi
@@ -9,16 +9,16 @@ Attr: abilityCard
 Method: getCombination, (double, next, half, useAbility (pure virtual)) 
 */
 
-class playerAction : public player {
+class PlayerAction : public player {
     public:
     // Getter
-    int getCombination(); 
+    int getCombination(); // Menampilkan kombinasi yang mungkin dari kartu yang dimiliki oleh pemain
 
-    void nextAction(); // 
-    void doubleAction();
-    void halfAction();
+    void nextAction(); // Aksi dimana tidak terjadi perubahan poin game dan langsung menuju pemain selanjutnya
+    void doubleAction(); // Aksi dimana poin game menjadi dua kali lipat dari sebelumnya
+    void halfAction(); // Aksi dimana poin game menjadi setengahnya
 
-    virtual void useAbility() = 0;
+    virtual void useAbility() = 0; // Aksi dimana pemain menggunakan kartu ability yang dimilikinya
 };
 
 #endif
