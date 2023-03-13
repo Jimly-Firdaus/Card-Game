17b5a72255c109e6e26def06554bbbdd42073e40
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-14 00:41:09 +0700

    refactor: revert main schema

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
-#include "GameState.hpp"
+#include "Combination/Combination.hpp"
+#include "Ability/Ability.hpp"
+#include "GameState/GameState.hpp"
 
 int main()
 {
-    GameState x;
-    cout << x.getRewardPoint() << endl;
-    // x.showTableCard();
-    cout << x.getCurrentTurn() << endl;
+    bool terminateGame = false;
 
-    for (int i = 0; i < 10; i++) {
-        x.nextPlayerOrder();
-        cout << x.getCurrentTurn() << endl;
-    }
-    
-    x.setReverseStatus(true);
-    x.setReverseStatusChange(true);
-    
-    for (int i = 0; i < 10; i++) {
-        x.nextPlayerOrder();
-        cout << x.getCurrentTurn() << endl;
+    while (!terminateGame)
+    {
+        // Player + game state instantiation here
+
+        // Game loop
+        bool foundWinner = false;
+        while (!foundWinner)
+        {
+            int currentRound = 0;
+
+            // Round loop
+            while (currentRound != 7)
+            {
+
+                currentRound++;
+            }
+        }
     }
 
-    x.setReverseStatus(false);
-    x.setReverseStatusChange(true);
-    for (int i = 0; i < 10; i++) {
-        x.nextPlayerOrder();
-        cout << x.getCurrentTurn() << endl;
-    }
+    return 0;
 }
