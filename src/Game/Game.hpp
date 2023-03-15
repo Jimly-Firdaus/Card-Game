eecf7718dc33738658de5e70d53b392a73df1dad
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-16 00:35:36 +0700

    feat(game): game object interface

---
+++ src/Game/Game.hpp
@@ -0,0 +1,36 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "../GameState/GameState.hpp"
+// #include "player/player.hpp"
+// #include "player/playerAction.hpp"
+#include "../Player/PlayerCollection.hpp"
+#include "../Comparator/Comparator.hpp"
+
+class Game {
+    private:
+        bool isEndGame;
+        int currentRound;
+        bool fileConfig;
+        vector<char> baseCardColor;
+        vector<string> baseAbility;
+        vector<string> availableCommands;
+
+    public:
+        Game();
+        void startGame();
+        void endGame();
+        void setBaseAbility(vector<string> baseAbility);
+        void setEndGameStatus(bool status);
+        bool validateUsername(vector<PlayerAction> players, string newUsername);
+        void resetRound();
+        void nextRound();
+        void restartGame();
+        vector<char> getBaseCardColor();
+        vector<string> getBaseAbility();
+        bool endCurrentGame(vector<PlayerAction> players);
+        void printGameResult(vector<PlayerAction> players);
+        void getCommand(string& command);
+};
+
+#endif
