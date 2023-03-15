cd8fbf3d7b538c9d654d8524a7a66ab1859318cf
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-03-15 20:03:56 +0700

    fix readFile

--- src/Deck/mainDeck.cpp
+++ src/Deck/mainDeck.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main()
 {
     Deck d1;
-    vector<pair<int, char> > newCards = d1.cardFromFile("testText");
-    d1.setCards(newCards);
+    pair<vector<pair<int, char> >, vector<string> > newCards = d1.cardFromFile("testText");
+    vector<pair<int, char> > deckCard = newCards.first;
+    vector<string> abilityCard = newCards.second;
+    d1.setCards(deckCard);
     d1.printCard();
     // d1.shuffleCard();
     d1.printCard();
+    /*
     cout << "--------------------------------\n";
     Deck d2;
     pair<int, char> newElmt = make_pair(10, 'X');
@@ -19,6 +22,13 @@ int main()
     newElmt = make_pair(1, 'M');
     d3 = d2 - newElmt;
     d3.printCard();
+    */
+
+    cout << "----------- Ability -------------\n";
+    for (int i = 0; i < abilityCard.size(); i++)
+    {
+        cout << i + 1 << ". <" << abilityCard[i] << ">" << endl;
+    }
 
     return 0;
 }
