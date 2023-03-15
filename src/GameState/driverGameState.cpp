b60a4cc35069f89de99a3d2c01b82efe185e9663
billc27 <110593711+billc27@users.noreply.github.com>
2023-03-15 21:12:38 +0700

    updated driver

--- src/GameState/driverGameState.cpp
+++ src/GameState/driverGameState.cpp
@@ -6,9 +6,53 @@
 int main()
 {
     GameState x;
-    // cout << x.getRewardPoint() << endl;
-    // // x.showTableCard();
-    // cout << x.getCurrentTurn() << endl;
+    Player a1, a2, a3, a4, a5, a6, a7;
+
+    a1.setPlayerPoint(20000);
+    a2.setPlayerPoint(20000);
+    a3.setPlayerPoint(20000);
+    a4.setPlayerPoint(4300000050);
+    a5.setPlayerPoint(20000);
+    a6.setPlayerPoint(20000);
+    a7.setPlayerPoint(20000);
+
+    vector<Player> x1;
+    x1.push_back(a1);
+    x1.push_back(a2);
+    x1.push_back(a3);
+    x1.push_back(a4);
+    x1.push_back(a5);
+    x1.push_back(a6);
+    x1.push_back(a7);
+
+    if (x.isWin(x1)) {
+        cout << "Win" << endl;
+    } else {
+        cout << "Lose" << endl;
+    }
+
+    a1.setPlayerPoint(20000);
+    a2.setPlayerPoint(20000);
+    a3.setPlayerPoint(20000);
+    a4.setPlayerPoint(20000);
+    a5.setPlayerPoint(20000);
+    a6.setPlayerPoint(20000);
+    a7.setPlayerPoint(20000);
+
+    vector<Player> x2;
+    x2.push_back(a1);
+    x2.push_back(a2);
+    x2.push_back(a3);
+    x2.push_back(a4);
+    x2.push_back(a5);
+    x2.push_back(a6);
+    x2.push_back(a7);
+
+    if (x.isWin(x2)) {
+        cout << "Win" << endl;
+    } else {
+        cout << "Lose" << endl;
+    }
 
     for (int i = 0; i < 7; i++) {
         cout << x.getCurrentTurn() << " ";
