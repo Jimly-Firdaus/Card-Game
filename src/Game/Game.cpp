0e39ca1383231c30094008328c1dfa88d6635692
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-16 17:42:55 +0700

    fix(game): cout endline

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,13 +1,15 @@
 #include "Game.hpp"
 
-Game::Game() : baseCardColor{'M', 'K', 'B', 'H'}, baseAbility{"Re-Roll", "Quadruple", "Quarter", "ReverseDirection", "SwapCard", "Switch", "AbilityLess"} {
+Game::Game() : baseCardColor{'M', 'K', 'B', 'H'}, baseAbility{"Re-Roll", "Quadruple", "Quarter", "ReverseDirection", "SwapCard", "Switch", "AbilityLess"}
+{
     this->isEndGame = false;
     this->fileConfig = false;
     vector<string> availableCommands = {"NEXT", "RE-ROLL", "DOUBLE", "QUADRUPLE", "HALF", "QUARTER", "REVERSE", "SWAPCARD", "SWITCH", "ABILITYLESS"};
     this->availableCommands = availableCommands;
 }
 
-void Game::startGame() {
+void Game::startGame()
+{
     // random seed
     srand(time(NULL));
 
@@ -16,17 +18,17 @@ void Game::startGame() {
         // Player collection + game state instantiation here
         vector<PlayerAction> players;
         // Set up player
-        for(int i= 0; i< 7; i++){
+        for (int i = 0; i < 7; i++)
+        {
             PlayerAction player;
-            string nickname;    
-            do {
+            string nickname;
+            do
+            {
                 cout << "Enter username [" << i + 1 << "]: ";
-                // getline(cin, nickname);
                 cin >> nickname;
                 cin.ignore();
             } while (!validateUsername(players, nickname));
             player.setNickName(nickname);
-            // player.getCard(gameDeck);
             players.push_back(player);
         }
         GameState gamestate;
@@ -36,178 +38,200 @@ void Game::startGame() {
         {
             // card input configuration
             string choice;
-            do {
+            do
+            {
                 cout << "Input File ? (y/n): ";
                 cin >> choice;
-                if (choice != "y" && choice != "n") cout << "\nPlease check your input!\n";
-                // cout << "---" << choice << "---\n";
+                cin.ignore();
+                if (choice == "y")
+                    cout << "Enter file name (.txt): ";
+                if (choice != "y" && choice != "n")
+                    cout << "\nPlease check your input!\n";
             } while (choice != "y" && choice != "n");
             this->fileConfig = choice == "y";
 
             // Reinstantiate state & deck
             Deck gameDeck;
-            if (!this->fileConfig) {
-                // Refill & Reshuffle deck 
-                for(int i= 0; i< 4; i++){
-                    for(int j= 1; j<=13; j++){
-                        pair<int, char> card= make_pair(j, getBaseCardColor()[i]);
+            if (!this->fileConfig)
+            {
+                // Refill & Reshuffle deck
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 1; j <= 13; j++)
+                    {
+                        pair<int, char> card = make_pair(j, getBaseCardColor()[i]);
                         gameDeck + card;
                     }
                 }
                 gameDeck.shuffleCard();
-            // cout << "SIGSEGVVVVVVV - if" << endl;
-            } else {
-                // cout << "SIGSEGVVVVVVV - else" << endl;
+            }
+            else
+            {
                 // Get card config from txt
                 string filename;
-                // getline(cin, filename);
                 cin >> filename;
                 cin.ignore();
                 vector<pair<int, char>> deckCards;
-                try {
+                try
+                {
                     auto temp = gameDeck.cardFromFile(filename);
                     deckCards = temp.first;
                     setBaseAbility(temp.second);
-                } catch (exception &e) {
-                    cout << e.what() << " Exiting Program...\n" << endl;
+                }
+                catch (exception &e)
+                {
+                    cout << e.what() << " Exiting Program...\n"
+                         << endl;
                     exit(0);
                 }
                 gameDeck = deckCards;
             }
-            // cout << "SIGSEGVVVVVVV - player" << endl;
-            for (auto& player : players) {
+            for (auto &player : players)
+            {
                 player.getCard(gameDeck);
             }
-            // cout << "SIGSEGVVVVVVV - after-player" << endl;
             // Round loop
             resetRound();
             restoreAbilityCard();
             gamestate.reset();
-            // int currentRound = 1;
-            // cout << "SEGG FAULTT" << endl;
             while (currentRound < 7)
             {
-                cout << "--------- NEW ROUND --------" << endl;
+                cout << "\n\n--------- ROUND: " << currentRound << " --------\n"
+                     << endl;
                 // if round 2 then distribute ability card
-                if(currentRound == 2){
-                    for(int i = 0; i < players.size(); i++){
+                if (currentRound == 2)
+                {
+                    for (int i = 0; i < players.size(); i++)
+                    {
                         players[i].getAbilityCard(this->baseAbility);
                     }
                 }
-                if (currentRound >= 2) {
+                if (currentRound >= 2)
+                {
                     gamestate.addTableCard(gameDeck);
                 }
                 // play loop
                 int maxTurn = 0;
-                while (maxTurn < 7) {
-                    cout << "Table card: \n";
-                    gamestate.getTableCard().printCard();
-                    cout << "\nCurrent Player Ability: " << players[gamestate.getCurrentTurn()-1].getAbility();
-                    cout << endl;
-                    cout << "Urutan sekarang: ";
-                    gamestate.showPlayerOrder();
-                    cout << endl;
+                while (maxTurn < 7)
+                {
                     cout << "Reward Point: " << gamestate.getRewardPoint();
                     cout << endl;
-                    players[gamestate.getCurrentTurn()-1].playerPlay(gameDeck, gamestate, players);
+                    cout << "Player order: ";
+                    gamestate.showPlayerOrder();
+                    cout << endl;
+                    cout << "Table card: \n";
+                    gamestate.getTableCard().printCard();
+                    players[gamestate.getCurrentTurn() - 1].playerPlay(gameDeck, gamestate, players);
                     gamestate.nextPlayerOrder();
                     maxTurn++;
                 }
-                cout << "--------Round: " << currentRound << "-----------" << endl;
-                // cout << "HERE" << endl;
-                if (currentRound == 6) {
-                    // cout << "HERE";
-                    // bool haveWinner = endCurrentGame(players);
+                if (currentRound == 6)
+                {
                     vector<Combination> playersCombination;
-                    for (auto player : players) {
+                    for (auto player : players)
+                    {
                         Combination c(gamestate.getTableCard(), player.getOwnedCard(), player.getNickName());
                         playersCombination.push_back(c);
                     }
                     Comparator<Combination> comparator;
                     pair<string, string> result = comparator.compare(playersCombination);
-                    for (auto& player : players) {
-                        if (player.getNickName() == result.first) {
+                    for (auto &player : players)
+                    {
+                        if (player.getNickName() == result.first)
+                        {
                             player.setPlayerPoint(gamestate.getRewardPoint());
                         }
                     }
+                    cout << endl;
                     cout << result.first << " win this game with " << result.second << endl;
                 }
-                cout << "FINISHED ROUND : " << currentRound << endl;
                 nextRound();
-                // currentRound++;
             }
-            if (endCurrentGame(players)) {
+            if (endCurrentGame(players))
+            {
                 foundWinner = true;
             }
         }
-        // Found Winner 
-        // Get Winner Data
-        // Prompt All Player scores here
+        // Found Winner
+        cout << "\nGAME SCORES: \n";
         printGameResult(players);
-        try {
+        try
+        {
             restartGame();
-        } catch (exception &e) {
+        }
+        catch (exception &e)
+        {
             cout << e.what() << " The game will end shortly...";
             endGame();
         }
     }
-
 }
 
-void Game::endGame() {
+void Game::endGame()
+{
     cout << "Bye!" << endl;
     setEndGameStatus(true);
 }
 
-void Game::setEndGameStatus(bool status) {
+void Game::setEndGameStatus(bool status)
+{
     this->isEndGame = status;
 }
 
-void Game::resetRound() {
+void Game::resetRound()
+{
     this->currentRound = 1;
 }
 
-void Game::nextRound() {
+void Game::nextRound()
+{
     this->currentRound++;
 }
 
-void Game::restoreAbilityCard() {
+void Game::restoreAbilityCard()
+{
     vector<string> baseAbility = {"Re-Roll", "Quadruple", "Quarter", "ReverseDirection", "SwapCard", "Switch", "AbilityLess"};
     this->baseAbility = baseAbility;
 }
 
-void Game::restartGame() {
+void Game::restartGame()
+{
     string input;
-    cout << "Restart? :\n";
+    cout << "\nRestart? :\n";
     cout << "\t1. Ya\n";
     cout << "\t2. Tidak\n";
-    // getline(cin, input);
+    cout << "Choice: ";
     cin >> input;
-    if (input == "1" || input == "2") {
-        if (input == "2") {
+    if (input == "1" || input == "2")
+    {
+        if (input == "2")
+        {
             endGame();
         }
-    } else {
+    }
+    else
+    {
         InvalidInput e;
         throw e;
     }
-    
 }
 
-vector<char> Game::getBaseCardColor() {
+vector<char> Game::getBaseCardColor()
+{
     return this->baseCardColor;
 }
 
-vector<string> Game::getBaseAbility() const {
+vector<string> Game::getBaseAbility() const
+{
     return this->baseAbility;
 }
 
-bool Game::validateUsername(vector<PlayerAction> players, string newUsername) {
-    // cout << newUsername << endl;
-    for (auto player : players) {
-        if (player.getNickName() == newUsername) {
-            // cout << player.getNickName() << endl;
-            // cout << newUsername << endl;
+bool Game::validateUsername(vector<PlayerAction> players, string newUsername)
+{
+    for (auto player : players)
+    {
+        if (player.getNickName() == newUsername)
+        {
             cout << "\nUsername already taken!\n";
             return false;
         }
@@ -215,11 +239,14 @@ bool Game::validateUsername(vector<PlayerAction> players, string newUsername) {
     return true;
 }
 
-bool Game::endCurrentGame(vector<PlayerAction> players) {
+bool Game::endCurrentGame(vector<PlayerAction> players)
+{
     bool result = false;
     int i = 0;
-    while (!result && i < players.size()) {
-        if (players[i].getPlayerPoint() >= pow(2, 32)) {
+    while (!result && i < players.size())
+    {
+        if (players[i].getPlayerPoint() >= pow(2, 32))
+        {
             result = true;
         }
         i++;
@@ -227,34 +254,43 @@ bool Game::endCurrentGame(vector<PlayerAction> players) {
     return result;
 }
 
-void Game::printGameResult(vector<PlayerAction> players) {
+void Game::printGameResult(vector<PlayerAction> players)
+{
     Comparator<PlayerAction> comparator;
     comparator.sortVector(players, false);
     int index = 0;
-    for (auto player : players) {
+    for (auto player : players)
+    {
         cout << "\t" << index + 1 << ". " << player.getNickName() << " : " << player.getPlayerPoint() << endl;
+        index++;
     }
-    cout << "Winner: " << players[0].getNickName() << endl;
+    cout << "\nWinner: " << players[0].getNickName() << endl;
 }
 
-void Game::getCommand(string& command) {
+void Game::getCommand(string &command)
+{
     int counter = 0;
     bool validCommand = false;
-    do{
+    do
+    {
         cout << "Command: ";
-        // getline(cin, command);
         cin >> command;
         cin.ignore();
         auto it = find(this->availableCommands.begin(), this->availableCommands.end(), command);
-        if (it != this->availableCommands.end()) {
+        if (it != this->availableCommands.end())
+        {
             validCommand = true;
-        } else {
+        }
+        else
+        {
             validCommand = false;
         }
-        if (!validCommand) cout << "Please check your input!" << endl;
+        if (!validCommand)
+            cout << "Please check your input!" << endl;
     } while (!validCommand);
 }
 
-void Game::setBaseAbility(vector<string> baseAbility) {
+void Game::setBaseAbility(vector<string> baseAbility)
+{
     this->baseAbility = baseAbility;
 }
--- src/Game/Game.hpp
+++ src/Game/Game.hpp
@@ -2,35 +2,35 @@
 #define GAME_HPP
 
 #include "../GameState/GameState.hpp"
-// #include "../Player/PlayerCollection.hpp"
 #include "../Comparator/Comparator.hpp"
 #include <ctime>
 
-class Game {
-    private:
-        bool isEndGame;
-        int currentRound;
-        bool fileConfig;
-        vector<char> baseCardColor;
-        vector<string> baseAbility;
-        vector<string> availableCommands;
+class Game
+{
+private:
+    bool isEndGame;
+    int currentRound;
+    bool fileConfig;
+    vector<char> baseCardColor;
+    vector<string> baseAbility;
+    vector<string> availableCommands;
 
-    public:
-        Game();
-        void startGame();
-        void endGame();
-        void setBaseAbility(vector<string> baseAbility);
-        void setEndGameStatus(bool status);
-        void restoreAbilityCard();
-        bool validateUsername(vector<PlayerAction> players, string newUsername);
-        void resetRound();
-        void nextRound();
-        void restartGame();
-        vector<char> getBaseCardColor();
-        vector<string> getBaseAbility() const;
-        bool endCurrentGame(vector<PlayerAction> players);
-        void printGameResult(vector<PlayerAction> players);
-        void getCommand(string& command);
+public:
+    Game();
+    void startGame();
+    void endGame();
+    void setBaseAbility(vector<string> baseAbility);
+    void setEndGameStatus(bool status);
+    void restoreAbilityCard();
+    bool validateUsername(vector<PlayerAction> players, string newUsername);
+    void resetRound();
+    void nextRound();
+    void restartGame();
+    vector<char> getBaseCardColor();
+    vector<string> getBaseAbility() const;
+    bool endCurrentGame(vector<PlayerAction> players);
+    void printGameResult(vector<PlayerAction> players);
+    void getCommand(string &command);
 };
 
 #endif
