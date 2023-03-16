ac5cec07caf5d0a22d6d1f7d644ca76c3efa98bb
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-16 12:29:41 +0700

    feat(game): run batch

---
+++ src/run.bat
@@ -0,0 +1,1 @@
+g++ main.cpp Game/Game.cpp player/playerAction.cpp player/player.cpp player/PlayerCollection.cpp GameState/GameState.cpp Deck/Deck.cpp Comparator/Comparator.cpp Comparator/Handler.cpp Combination/Combination.cpp -o a
